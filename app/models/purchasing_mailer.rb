4b17be51752a1dfc7553000da4e385d64f0ed58d
Rahmat Budiharso <rbudiharso@gmail.com>
2010-07-23 14:46:37 +0700

    added quotation mailer

--- app/models/purchasing_mailer.rb
+++ app/models/purchasing_mailer.rb
@@ -1,6 +1,10 @@
 class PurchasingMailer < ActionMailer::Base
 
-  def quotation_request(request)
-
+  def quotation_request(quotation)
+    recipients quotation.suppliers.collect { |supp| supp.email }
+    from "Purchasing Dept. #{quotation.company.name} <rbudiharso@gmail.com>"
+    subject "Quotation request"
+    sent_on Time.now
+    body :quotation => quotation
   end
 end
