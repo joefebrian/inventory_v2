1923443c5dc81959473f24a7dd9310a9ac2291f5
ones006 <ones006@gmail.com>
2010-07-23 08:07:06 +0700

    salesorderentries2

--- db/migrate/20100722185052_create_sales_orders.rb
+++ db/migrate/20100722185052_create_sales_orders.rb
@@ -1,15 +1,15 @@
 class CreateSalesOrders < ActiveRecord::Migration
   def self.up
     create_table :sales_orders do |t|
-      t.integer, :company_id
-      t.integer, :quotation_id
-      t.string, :number
-      t.date, :tanggal
-      t.integer, :top
-      t.integer, :advance
-      t.integer, :status
-      t.integer, :totral_bruto
-      t.integer, :total_disc
+      t.integer :company_id
+      t.integer :quotation_id
+      t.string :number
+      t.date :tanggal
+      t.integer :top
+      t.integer :advance
+      t.integer :status
+      t.integer :totral_bruto
+      t.integer :total_disc
       t.integer :total_netto
       t.timestamps
     end
--- db/migrate/20100723005948_create_sales_order_entries.rb
+++ db/migrate/20100723005948_create_sales_order_entries.rb
@@ -1,8 +1,8 @@
 class CreateSalesOrderEntries < ActiveRecord::Migration
   def self.up
     create_table :sales_order_entries do |t|
-      t.integer, :sales_order_id
-      t.string, :item_id
+      t.integer :sales_order_id
+      t.string :item_id
       t.integer :quantity
 
       t.timestamps
