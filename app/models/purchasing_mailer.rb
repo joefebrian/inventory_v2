4898e15d8b0a118aa0acd1687592882e9b716a60
Rahmat Budiharso <rbudiharso@gmail.com>
2010-07-26 14:38:57 +0700

    added reply_to in purchasing mailer

--- app/models/purchasing_mailer.rb
+++ app/models/purchasing_mailer.rb
@@ -2,7 +2,8 @@ class PurchasingMailer < ActionMailer::Base
 
   def quotation_request(quotation, supplier)
     recipients supplier.email
-    from "Purchasing Dept. #{quotation.company.name} <rbudiharso@tudulis.com>"
+    from "Purchasing Dept. #{quotation.company.name} <purchasing@cycomsoft.com>"
+    reply_to "purchasing@cycomsoft.com"
     subject "Quotation request"
     sent_on Time.now
     body :quotation => quotation, :supplier => supplier
