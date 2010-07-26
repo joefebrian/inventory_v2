f6fdb4b982604e35feb677976a39fee1f229f4c1
ones006 <ones006@gmail.com>
2010-07-26 17:20:00 +0700

    edit so

---
+++ db/migrate/20100726101348_add_retensi_to_sales_order.rb
@@ -0,0 +1,15 @@
+class AddRetensiToSalesOrder < ActiveRecord::Migration
+  def self.up
+    add_column :sales_orders, :retensi, :string
+    add_column :sales_orders, :customer_id, :integer
+    add_column :sales_orders, :order_ref, :string
+    add_column :sales_orders, :kurs_id, :integer
+  end
+
+  def self.down
+    remove_column :sales_orders, :kurs_id
+    remove_column :sales_orders, :order_ref
+    remove_column :sales_orders, :customer_id
+    remove_column :sales_orders, :retensi
+  end
+end
