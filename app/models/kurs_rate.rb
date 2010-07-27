b10a40ab29131a38c09fb55364a7bb0bf6c50e60
ones006 <ones006@gmail.com>
2010-07-27 13:56:24 +0700

    hapus schema.rb

--- app/models/kurs_rate.rb
+++ app/models/kurs_rate.rb
@@ -1,4 +1,8 @@
 class KursRate < ActiveRecord::Base
+  validates_presence_of :code
+  validates_presence_of :name
+  validates_presence_of :value
+  validates_uniqueness_of :code
   has_many :sales_order
   belongs_to :company
 end
