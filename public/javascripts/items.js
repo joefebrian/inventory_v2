82048f5e3a16d54b600eeb59e21e13532ffb77f1
Rahmat Budiharso <rbudiharso@gmail.com>
2010-09-30 18:59:25 +0700

    fix items.js

--- public/javascripts/items.js
+++ public/javascripts/items.js
@@ -20,3 +20,24 @@ $('#units li input[name*=conversion_rate]').live('keyup', function() {
     $(this).parent('li').find('input[name*=value]').val(first_unit_value * parseInt(value));
   }
 });
+
+$("a#add_unit").live('click', function() {
+  $(this).parent().before(new_unit_fields(unit_length));
+  unit_length = $('#units ol li').length - 1;
+  return false;
+});
+
+function new_unit_fields(n) {
+  var html = "<li> "+
+    "<label for=\"item_units_attributes_"+n+"_name\">Name</label> "+
+    "<input type=\"text\" value=\"\" size=\"20\" name=\"item[units_attributes]["+n+"][name]\" id=\"item_units_attributes_"+n+"_name\"> "+
+    "<label for=\"item_units_attributes_"+n+"_conversion_rate\">Conversion rate</label> "+
+    "<input type=\"text\" value=\"\" size=\"5\" name=\"item[units_attributes]["+n+"][conversion_rate]\" id=\"item_units_attributes_"+n+"_conversion_rate\" class=\"number\"> "+
+    "<label for=\"item_units_attributes_"+n+"_value\">Value</label> "+
+    "<input type=\"text\" size=\"13\" name=\"item[units_attributes]["+n+"][value]\" id=\"item_units_attributes_"+n+"_value\" class=\"number\"> "+
+    "<input type=\"hidden\" name=\"item[units_attributes]["+n+"][_destroy]\" id=\"item_units_attributes_"+n+"__destroy\"> "+
+    "<a class=\"unit_remover\" title=\"remove unit\" href=\"#\"><img src=\"/images/icons/silk/cross.png\" alt=\"Cross\"></a> "+
+    "</li>";
+  return html;
+}
+
