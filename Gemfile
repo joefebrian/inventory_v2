b79d0c25aa59c89065d16d6352fe981e92ce77d0
Rahmat Budiharso <rbudiharso@gmail.com>
2010-10-15 14:09:40 +0700

    using bundler for gem management

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source :gemcutter
+gem "rails", "~> 2.3.10"
+gem "mysql"
+gem "prawn"
+gem "mongrel"
+gem "cancan"
+gem "chronic"
+gem "slim_scrooge"
+gem "spreadsheet"
+gem "will_paginate", "~> 2.3.14"
+gem "authlogic"
+gem "searchlogic"
+gem "subdomain-fu"
+gem "formtastic"
+gem "haml"
+
+# bundler requires these gems in all environments
+# gem "nokogiri", "1.4.2"
+# gem "geokit"
+
+group :development do
+  # bundler requires these gems in development
+  # gem "rails-footnotes"
+  gem "bullet"
+end
+
+group :test do
+  # bundler requires these gems while running tests
+  # gem "rspec"
+  # gem "faker"
+end
--- config/environment.rb
+++ config/environment.rb
@@ -19,17 +19,17 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'will_paginate', :version => '2.3.14'
-  config.gem 'authlogic'
-  config.gem 'searchlogic'
-  config.gem 'subdomain-fu'
-  config.gem 'slim_scrooge'
-  config.gem 'cancan'
-  config.gem 'formtastic'
-  config.gem 'chronic'
-  config.gem 'prawn'
-  config.gem 'haml'
-  config.gem 'spreadsheet'
+  #config.gem 'will_paginate', :version => '2.3.14'
+  #config.gem 'authlogic'
+  #config.gem 'searchlogic'
+  #config.gem 'subdomain-fu'
+  #config.gem 'slim_scrooge'
+  #config.gem 'cancan'
+  #config.gem 'formtastic'
+  #config.gem 'chronic'
+  #config.gem 'prawn'
+  #config.gem 'haml'
+  #config.gem 'spreadsheet'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
