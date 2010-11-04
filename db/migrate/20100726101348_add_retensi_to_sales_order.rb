42d0960a867b84b4ff12a497c9e45bf34e848313
Rahmat Budiharso <rbudiharso@gmail.com>
2010-11-04 12:37:06 +0700

    fix a migration file

--- db/migrate/20100726101348_add_retensi_to_sales_order.rb
+++ db/migrate/20100726101348_add_retensi_to_sales_order.rb
@@ -1,7 +1,7 @@
 class AddRetensiToSalesOrder < ActiveRecord::Migration
   def self.up
     add_column :sales_orders, :retensi, :string
-    add_column :sales_orders, :customer_id, :integer
+    #add_column :sales_orders, :customer_id, :integer
     add_column :sales_orders, :order_ref, :string
     add_column :sales_orders, :kurs_id, :integer
   end
@@ -9,7 +9,7 @@ class AddRetensiToSalesOrder < ActiveRecord::Migration
   def self.down
     remove_column :sales_orders, :kurs_id
     remove_column :sales_orders, :order_ref
-    remove_column :sales_orders, :customer_id
+    #remove_column :sales_orders, :customer_id
     remove_column :sales_orders, :retensi
   end
 end
