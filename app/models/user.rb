bfa525e6defc3d86a4f512cb6befef7bb5ef9d84
Rahmat Budiharso <rbudiharso@gmail.com>
2011-05-06 11:06:06 +0700

    added validation scope to authlogic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  acts_as_authentic
+  acts_as_authentic do |c|
+    c.validations_scope = :company_id
+  end
   attr_accessible :username, :email, :password, :password_confirmation, :role_ids, :company_id
   belongs_to :company
   has_and_belongs_to_many :roles
